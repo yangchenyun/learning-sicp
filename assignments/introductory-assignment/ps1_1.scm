e52a6be428f6914dd22e0b326550e0fb662a3a90
Steven Yang <yangchenyun@gmail.com>
2012-08-09 00:00:23 +0800

    Initial commit

---
+++ assignments/introductory-assignment/ps1_1.scm
@@ -0,0 +1,34 @@
+;;; This is the code for ps1.
+
+
+(define p1
+  (lambda (x y)
+    (+ (p2 x y) (p3 x y))))
+
+(define p2
+  (lambda (z w)
+    (* z w)))
+
+(define p3
+  (lambda (a b)
+    (+ (p2 a) (p2 b))))
+
+(define fold
+  (lambda (x y)
+    (* (spindle x)
+       (+ (mutilate y)
+	  (spindle x)))))
+
+(define spindle
+  (lambda (w) (* w w)))
+
+(define mutilate
+  (lambda (z)
+    (+ (spindle z) z)))
+
+(define fact
+  (lambda (n)
+    (if (= n 0)
+	(* n (fact (- n 1))))))
+
+
